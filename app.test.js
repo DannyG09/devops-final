863d6457a0418bec1fbffb0949189cd77348f3e0
DannyG09 <dannyguerrero.m53@gmail.com>
2025-12-12 14:43:00 -0400

    Se actualiza test para que coincida con el mensaje real

--- app.test.js
+++ app.test.js
@@ -5,6 +5,6 @@ describe("GET /", () => {
   it("Debe devolver Hola Mundo", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("Hola Mundo desde DevOps CI/CD!");
+    expect(res.text).toBe("Hola Mundo desde DevOps Danny Enmanuel Guerrero Martinez 2024-0092 , deploy automático funcionando!!");
   });
 });
