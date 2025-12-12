74a0b4bba75d4353bcd297478bb989c804716f49
DannyG09 <dannyguerrero.m53@gmail.com>
2025-12-12 14:39:50 -0400

    Prueba de CI/CD completo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola Mundo desde DevOps Danny Enmanuel Guerrero Martinez 2024-0092 CI/CD!");
+  res.send("Hola Mundo desde DevOps Danny Enmanuel Guerrero Martinez 2024-0092 , deploy automático funcionando!!");
 });
 
 if (require.main === module) {
